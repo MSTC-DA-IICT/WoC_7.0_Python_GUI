4d50778182a3e61f9e5a3e3e3e3dbc02ddcf27a2
VrundDA <202301015@daiict.ac.in>
2024-12-28 14:00:22 +0530

    Update EditRestaurant.py

--- EditRestaurant.py
+++ EditRestaurant.py
@@ -3,6 +3,7 @@ from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
 import json
 import os
+import time
 
 class EditRestaurant_UI(QMainWindow):
     def __init__(self,dark_mode,MainWindow):
@@ -21,7 +22,6 @@ class EditRestaurant_UI(QMainWindow):
         self.Cancel = self.findChild(QPushButton,"pushButton_3")
         self.mylocaladdress = os.getcwd()
 
-        self.original = self.RestaurantName.text()
         if (dark_mode==True):
             self.dark_style = """
             QMainWindow {
@@ -75,7 +75,6 @@ class EditRestaurant_UI(QMainWindow):
                 border: 1px solid #5c5c5c;
                 background-color: #3c3c3c;
             }
-
             QSpinBox {
                 background-color: #3c3c3c;
                 color: #ffffff;
@@ -100,6 +99,7 @@ class EditRestaurant_UI(QMainWindow):
         self.fname = "Not Found"
         self.parentWindow = MainWindow
 
+
         with open(f"{self.mylocaladdress}\Restaurant List\{MainWindow.SelectRestaurant.currentText()}.json","r") as f:
             data = json.load(f)
             self.RestaurantName.setText(f"{data["RestaurantName"]}")
@@ -109,6 +109,9 @@ class EditRestaurant_UI(QMainWindow):
             self.pixmap = QPixmap(self.folder_name)
             self.myimagelabel.setPixmap(self.pixmap)
         self.original = self.RestaurantName.text()
+        self.original_GSTNumber = self.GSTNumber.text()
+        self.original_FSSAINumber = self.FSSAINumber.text()
+        self.original_image = self.folder_name
 
         # Connect the Buttons
         self.Cancel.clicked.connect(self.CancelButton)
@@ -125,21 +128,52 @@ class EditRestaurant_UI(QMainWindow):
             self.pixmap = QPixmap(self.fname[0])
             self.myimagelabel.setPixmap(self.pixmap)
     def SavingChanges(self):
-        data = {
-            "RestaurantName": self.RestaurantName.text(),
-            "GSTNumber": self.GSTNumber.text(),
-            "FSSAINumber": self.FSSAINumber.text(),
-            "LogoRestaurant": self.fname,
-            "Recipes": [],
-            "RawMaterials": []
-        }
+        for file_name in os.listdir(f"{self.mylocaladdress}"):
+            if file_name.endswith(f"{self.original}"):
+                # print(file_name)
+                self.RestaurantLister = file_name.split(f"{self.original}")
+                os.rename(file_name,f"{self.RestaurantLister[0]}{self.RestaurantName.text()}")
+                new_folder_name = f"{self.RestaurantLister[0]}{self.RestaurantName.text()}"
+                if (not file_name.startswith("Bill")) and file_name.endswith(f"{self.original}"):
+                    for file_name_edit in os.listdir(f"{self.mylocaladdress}\{new_folder_name}"):
+                        # print(f"{self.mylocaladdress}\{new_folder_name}\{file_name_edit}")
+                        with open(f"{self.mylocaladdress}\{new_folder_name}\{file_name_edit}","r") as f:
+                            data = json.load(f)
+                            data["RestaurantName"] = f"{self.RestaurantName.text()}"
+                            data["GSTNumber"] = f"{self.GSTNumber.text()}"
+                            data["FSSAINumber"] = f"{self.FSSAINumber.text()}"
+                            data["LogoRestaurant"] = f"{self.fname[0]}"
+                        with open(f"{self.mylocaladdress}\{new_folder_name}\{file_name_edit}","w") as f:
+                            json.dump(data,f,indent=4)
+                            f.write('\n')
+        with open(f"{self.mylocaladdress}\Restaurant List\{self.original}.json","r") as f:
+            data = json.load(f)
+            data["RestaurantName"] = f"{self.RestaurantName.text()}"
+            data["GSTNumber"] = f"{self.GSTNumber.text()}"
+            data["FSSAINumber"] = f"{self.FSSAINumber.text()}"
+            data["LogoRestaurant"] = f"{self.fname[0]}"
         # print(self.original)
         # print(f"{self.mylocaladdress}\Restaurant List\{self.original}.json")
         # print(self.parentWindow.SelectRestaurant.currentText())
         # print(self.RestaurantName.text())
-        if self.original!=self.RestaurantName.text():
+        if self.original!=self.RestaurantName.text() or self.original_GSTNumber!=self.GSTNumber.text() or self.original_FSSAINumber!=self.FSSAINumber.text() or self.original_image!=self.fname[0]:
             # os.remove(f"{self.mylocaladdress}\Restaurant List\{self.original}.json")
             current_index = self.parentWindow.SelectRestaurant.currentIndex()
+            os.remove(f"{self.mylocaladdress}\Restaurant List\{self.original}.json")
+            os.rename(f"{self.mylocaladdress}\Recipes\{self.original}",f"{self.mylocaladdress}\Recipes\{self.RestaurantName.text()}")
+            os.rename(f"{self.mylocaladdress}\Billings\{self.original}",f"{self.mylocaladdress}\Billings\{self.RestaurantName.text()}")
+            for file_name_edit in os.listdir(f"{self.mylocaladdress}\Billings\{self.RestaurantName.text()}"):
+                with open(f"{self.mylocaladdress}\Billings\{self.RestaurantName.text()}\{file_name_edit}","r") as f:
+                    data = json.load(f)
+                    data["RestaurantName"] = f"{self.RestaurantName.text()}"
+                    data["GSTNumber"] = f"{self.GSTNumber.text()}"
+                    data["FSSAINumber"] = f"{self.FSSAINumber.text()}"
+                    data["LogoRestaurant"] = f"{self.fname[0]}"
+                with open(f"{self.mylocaladdress}\Billings\{self.RestaurantName.text()}\{file_name_edit}","w") as f:
+                    json.dump(data,f,indent=4)
+                    f.write('\n')
+            os.rename(f"{self.mylocaladdress}\Purchase Receipts\{self.original}",f"{self.mylocaladdress}\Purchase Receipts\{self.RestaurantName.text()}")
+            
             self.parentWindow.SelectRestaurant.removeItem(current_index)
             self.parentWindow.SelectRestaurant.addItem(f"{self.RestaurantName.text()}")
             
